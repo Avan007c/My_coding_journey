47bb6f24aa36781809bc59c006958becc9a22992
Avan007c <avanthika.sharma07@gmail.com>
2025-07-25 03:06:06 +0530

    Add files via upload

---
+++ day01.py
@@ -0,0 +1,12 @@
+#Day1
+print ("Hello Universe,")
+print ("I'm starting my Coding Journey")
+print ("""
+           I'm excited
+           I'm signing up for Replit's 
+           100 days of Python challenge!
+           
+           Invited my friends to join
+           this course on replit too.
+           """)
+print ("Follow my progress at replit.com/@Avan007c")
